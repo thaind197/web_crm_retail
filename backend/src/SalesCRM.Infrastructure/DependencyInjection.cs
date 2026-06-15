a38fe9c97c43f23ebdd4fa07c21b2cc5ab144224
thaind <thaind@vmogroup.com>
2026-06-15 11:01:07 +0700

    fix(ui, backend): unify textbox styling and fix connection string URI decoding

--- backend/src/SalesCRM.Infrastructure/DependencyInjection.cs
+++ backend/src/SalesCRM.Infrastructure/DependencyInjection.cs
@@ -30,8 +30,8 @@ public static class DependencyInjection
             {
                 var uri = new Uri(connectionString);
                 var userInfo = uri.UserInfo.Split(':');
-                var user = userInfo[0];
-                var password = userInfo.Length > 1 ? userInfo[1] : "";
+                var user = Uri.UnescapeDataString(userInfo[0]);
+                var password = userInfo.Length > 1 ? Uri.UnescapeDataString(userInfo[1]) : "";
                 var host = uri.Host;
                 var port = uri.Port > 0 ? uri.Port : 5432;
                 var database = uri.AbsolutePath.TrimStart('/');
